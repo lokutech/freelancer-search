a67a83833cd75128cffa56e9fb788669b099069a
Onat <onatim@protonmail.com>
2021-02-26 13:04:22 -0600

    added multiple selection to location

--- src/LeftSidebar/Location.tsx
+++ src/LeftSidebar/Location.tsx
@@ -15,13 +15,9 @@ const useStyle = makeStyles({
   },
   root: {
     borderRadius: 0,
-    padding:'0 !important',
-    paddingLeft: '10 !important'
   },
   input: {
     width: 'auto !important',
-    paddingTop:'0 !important',
-    paddingBottom:'0 !important',
   },
 })
 
@@ -36,33 +32,33 @@ function Location() {
     <Container style={{ marginTop: '20px', overflow: 'auto' }}>
       <Typography style={{ fontWeight: 'bold' }}>Specific Location</Typography>
       <Autocomplete
-        classes={{
-          popupIndicator: classes.popupIndicator,
-          inputRoot: classes.input,
-        }}
-        // multiple
-        disableClearable
-        id="tags-outlined"
-        options={countries}
-        getOptionLabel={(option) => option.name}
-        filterSelectedOptions
-        style={{ background: 'red' }}
-        ChipProps={{
-          size: 'small',
-          variant: 'outlined',
-          classes: {
-            root: classes.root,
-          },
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="outlined"
-            // placeholder="Enter a skill"
-            style={{ background: 'white' , paddingLeft: '20'}}
-          />
-        )}
-      />
+          classes={{
+            popupIndicator: classes.popupIndicator,
+            input: classes.input,
+          }}
+          multiple
+          disableClearable
+          id="tags-outlined"
+          options={countries}
+          getOptionLabel={(option) => option.name}
+          filterSelectedOptions
+          style={{ background: 'red' }}
+          ChipProps={{
+            size: 'small',
+            variant: 'outlined',
+            classes: {
+              root: classes.root,
+            },
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              placeholder="Enter location"
+              style={{ background: 'white' }}
+            />
+          )}
+        />
       <Box m={2}></Box>
       <Button
         onClick={handleDelete}
